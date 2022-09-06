9df9d69c4e3af56b3cc255dd21137f850dbc9855
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-09-06 07:42:09 +0700

    Update mail.php

--- pages/mail.php
+++ pages/mail.php
@@ -6,9 +6,8 @@ $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "mormleapsovann555@mail.com";
 $subject = "Mail From website";
-$txt ="First Name: ". $firstname . "\r\nLast Name: " . $lastname ."\r\n  Email: " . $email . "\r\n Message: " . $message;
-$headers = "From: noreply@wolf3team.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$txt ="First Name: ". $firstname . "\r\nLast Name: " . $lastname ."\r\n  Email: " . $email . "\r\n Message: " . $message .;
+$headers = "From: noreply@wolf3team.com" .
 if(empty($firstname) || empty($lastname) || empty($email) || empty($message) || empty($to))
 {
     mail($to,$subject,$txt,$headers);
