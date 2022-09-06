a8a04b27b23f1de6fed48d21051fabc049b6b0af
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-09-06 07:28:07 +0700

    Update mail.php

--- pages/mail.php
+++ pages/mail.php
@@ -9,8 +9,12 @@ $subject = "Mail From website";
 $txt ="First Name: ". $firstname . "\r\nLast Name: " . $lastname ."\r\n  Email: " . $email . "\r\n Message: " . $message;
 $headers = "From: noreply@wolf3team.com" . "\r\n" .
 "CC: somebodyelse@example.com";
-if($email!=NULL){
+if(empty($firstname) || empty($lastname) || empty($email) || empty($message) || empty($to))
+{
     mail($to,$subject,$txt,$headers);
+    echo "<script typ='text/javascript'>alert('Message Sent Successfully!');
+        window.history.go(-1);
+        </script>";
 }
 //redirect
 header("Location:thankyou.html");
