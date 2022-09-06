b2bbc0341fa653f87d27426ffde9fde769c8b163
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-09-06 09:51:53 +0700

    Update mail.php

--- pages/mail.php
+++ pages/mail.php
@@ -6,22 +6,17 @@
     $message = $_POST['message']; 
     $to = 'mormleapsovann555@email.com'; 
     // $txt ="First Name: ". $firstname . "\r\nLast Name: " . $lastname ."\r\n  Email: " . $email . "\r\n Message: " . $message .;
-    $txt ="Name: " . $name ."\r\n  Email: " . $email . "\r\n Message: " . $message .;
     $headers = "From: noreply@wolf3team.com" .
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    if($_POST['submit']) {
-        if(mail($to,$subject,$txt,$headers)) {
+    // mail($to,$subject,$txt,$headers);
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $body, $email)) { 
             echo '<p style="color: #27ae60;">Your message has been sent!</p>';
-        } else {
-            echo '<p style="color:#c0392b;">Something went worng, go back and try again!</p>';
+        } else { 
+            echo '<p style="color: #c0392b;">Something went wrong, go back and try again!</p>'; 
         }
-            // mail($to,$subject,$txt,$headers);
-        // echo "<script type='text/javascript'>alert('Message Sent Successfully!');
-        //     window.history.go(-1);
-        //     </script>";
-    }
-    //redirect
-    header("Location:thankyou.html");
+    }    //redirect
+    // header("Location:thankyou.html");
 ?>
